2913bfd9761a84b37a7f4a8437645645f9d79661
Adryan Eka Vandra <adryanekavandra@gmail.com>
2019-01-26 11:16:24 +0700

    update readme.md

---
+++ api/modules/v1/controllers/NotificationAdminController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Project: event-hub.
+ * @author Adryan Eka Vandra <adryanekavandra@gmail.com>
+ *
+ * Date: 1/26/2019
+ * Time: 10:52 AM
+ */
+
+namespace api\modules\v1\controllers;
+
+
+use admin\models\NotificationAdmin;
+use common\extensions\auth\ApiAuth;
+use yii\rest\ActiveController;
+
+class NotificationAdminController extends ActiveController
+{
+    public $modelClass = NotificationAdmin::class;
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'items',
+    ];
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class'=>ApiAuth::className()
+        ];
+        $behaviors['contentNegotiator'] = [
+            'class' => \yii\filters\ContentNegotiator::className(),
+            'formats' => [
+                'application/json' => \yii\web\Response::FORMAT_JSON,
+            ],
+        ];
+        return $behaviors;
+    }
+}
