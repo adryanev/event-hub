24c7576c7900f2244f26a4847a114779989e4016
adryanev <adryanekavandra@gmail.com>
2019-02-23 19:46:53 +0700

    change vagrant setting for ubuntu

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,7 +4,7 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=event-hub',
-            'username' => 'root',
+            'username' => 'developer',
             'password' => '',
             'charset' => 'utf8',
         ],
